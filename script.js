75f1dec6abd0bc71ced8f737832dfc4c45473420
EthanLYM <149280583+EthanLYM@users.noreply.github.com>
2024-09-03 12:21:06 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbutton')) {
+      var dropdowns = document.getElementsByClassName("content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
