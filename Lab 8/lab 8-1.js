fbaabd4e1f6943437556f59d8f0446d1344fd575
EthanLYM <149280583+EthanLYM@users.noreply.github.com>
2024-10-17 12:50:02 +0800

    Update lab 8-1.js

--- Lab 8/lab 8-1.js
+++ Lab 8/lab 8-1.js
@@ -18,13 +18,13 @@ function init(){
                 .attr("height", h)
                 .attr("fill", "grey")
     
-    d3.json("Lab 8\LGA_VIC.json").then(function(json){
+    d3.json("LGA_VIC.json").then(function(json){
 
         svg.selectAll("path")
             .data(json.features)
             .enter()
             .append("path")
-            .attr("d", path);
+            .attr("d", path)
 
     });
 
